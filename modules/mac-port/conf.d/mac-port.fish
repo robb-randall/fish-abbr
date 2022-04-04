79beb45a6bd7a5ba71f7f1f136a6533193dc6be1
Robb Randall <robb.randall@gmail.com>
2022-04-03 19:10:01 -0500

    added req paths

--- modules/mac-port/conf.d/mac-port.fish
+++ modules/mac-port/conf.d/mac-port.fish
@@ -1,3 +1,11 @@
+set -l additional_paths \
+	/opt/local/bin \
+	/opt/local/sbin
+for path in $additional_paths
+	if test -d $path                                                    fish_add_path --path $path
+	end
+end
+
 abbr -ag pi		sudo port install
 abbr -ag pl		port list
 abbr -ag ps		port search
--- modules/ranger/conf.d/ranger.fish
+++ modules/ranger/conf.d/ranger.fish
@@ -1,1 +1,3 @@
 abbr -ag r	ranger
+abbr -ag rd	ranger --show-only-dirs
+abbr -ag rdh	ranger --show-only-dirs $HOME
