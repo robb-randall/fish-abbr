7e717a70233f7e6ba4ccceb41f9204bf88a9495c
Robb Randall <robb@void-live.Home>
2022-04-06 15:37:52 -0500

    added xbps

--- modules/ranger/conf.d/ranger.fish
+++ modules/ranger/conf.d/ranger.fish
@@ -1,4 +1,4 @@
 abbr -ag r	ranger
+abbr -ag rh	ranger $HOME
 abbr -ag rd	ranger --show-only-dirs
 abbr -ag rdh	ranger --show-only-dirs $HOME
-abbr -ag rh	ranger $HOME
---
+++ modules/xbps/conf.d/xbps.fish
@@ -0,0 +1,6 @@
+abbr -ag xi   sudo xbps-install
+abbr -ag xq   xbps-query -Rs
+abbr -ag xsb  $XBPS_SRC_ROOT/xbps-src pkg
+abbr -ag xsi  sudo xbps-install --repository $XBPS_SRC_ROOT/hostdir/sources
+abbr -ag xsq  ls $XBPS_SRC_ROOT/srcpkgs/
+abbr -ag xu   sudo xbps-install -Syu
