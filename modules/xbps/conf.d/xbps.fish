3fd15374e3aa17537ce7397808e466b23fd42fd4
Robb Randall <robb.randall@gmail.com>
2022-04-19 07:02:33 -0500

    updated xbps

--- modules/xbps/conf.d/xbps.fish
+++ modules/xbps/conf.d/xbps.fish
@@ -3,6 +3,8 @@ abbr -ag xq   	xbps-query -Rs
 abbr -ag xr	sudo xbps-remove
 abbr -ag xro	sudo xbps-remove -o
 abbr -ag xsb  	$XBPS_SRC_ROOT/xbps-src pkg
+abbr -ag xsc	$XBPS_SRC_ROOT/xbps-src show-sys-updates
 abbr -ag xsi  	sudo xbps-install --repository $XBPS_SRC_ROOT/hostdir/sources
+abbr -ag xsr	pushd $DBPS_SRC_ROOT && git pull && popd
 abbr -ag xsq  	ls $XBPS_SRC_ROOT/srcpkgs/ \| grep -i -e
 abbr -ag xu   	sudo xbps-install -Syu
