fb184b8ef3e3b8968bf066503036966e2c915b95
Robb Randall <robb.randall@gmail.com>
2022-06-16 22:52:05 -0500

    updated abbrs

---
+++ plugins/eopkg/conf.d/eopkg.fish
@@ -0,0 +1,9 @@
+abbr -ag eh	eopkg help
+abbr -ag ei	eopkg info
+abbr -ag eit	sudo eopkg install
+abbr -ag eli	eopkg list-installed
+abbr -ag ermo	sudo eopkg remove-orphans
+abbr -ag erm	sudo eopkg remove
+abbr -ag es	eopkg search
+abbr -ag eup	sudo eopkg upgrade
+abbr -ag eur	sudo eopkg update-repo
